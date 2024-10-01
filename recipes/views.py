2994397e016612f6f14ea36b1c336d579a8012c9
João Vítor <joao.diniz@maisunifacisa.com.br>
2024-10-01 11:43:17 -0300

    removed contact and about

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/home.html', context={
         "name": "João Vítor",
     })
-
-def contato(request):
-    return render(request, 'me-apague/temp.html')
-
-def sobre(request):
-    return HttpResponse("sobre")
